14d1364a745236101414f46e2cca8c96dec88417
Vineet Bhasin <bhasinvineet49@gmail.com>
2024-12-28 22:25:41 +0530

    Added a heart button, to be connected to backend

--- frontend/src/components/SolutionSection.jsx
+++ frontend/src/components/SolutionSection.jsx
@@ -1,14 +1,32 @@
 /* eslint-disable react/prop-types */
+import { useState } from "react";
 import { marked } from "marked";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart as faHeartSolid } from "@fortawesome/free-solid-svg-icons";
+import { faHeart as faHeartRegular } from "@fortawesome/free-regular-svg-icons";
 
-const SolutionSection = ({item, solutionTitle}) => {
+const SolutionSection = ({ item, solutionTitle }) => {
+  const [isSelected, setIsSelected] = useState(false);
   const renderedMarkdown = marked(item);
+
+  const toggleSelection = () => {
+    setIsSelected(!isSelected);
+  };
+
   return (
     <section className="suggested-solution-container" aria-live="polite">
-      <h2>{solutionTitle}</h2>
-      
+      <div className="solution-header">
+        <h2>{solutionTitle}</h2>
+        <button
+          className="heart-button"
+          aria-label="Like"
+          onClick={toggleSelection}
+          style={{ color: isSelected ? "#c92a2a" : "#000" }}
+        >
+          <FontAwesomeIcon icon={isSelected ? faHeartSolid : faHeartRegular} />
+        </button>
+      </div>
       <div dangerouslySetInnerHTML={{ __html: renderedMarkdown }} />
-  
     </section>
   );
 };
