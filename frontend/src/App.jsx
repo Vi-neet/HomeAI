40614b6f37a7bdb9ee2cefd80eee8036a8f6d80c
Vineet Bhasin <bhasinvineet49@gmail.com>
2025-01-02 12:19:44 +0530

    Moved navbar logic into Header.jsx from App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,36 +1,19 @@
-// import Header from "./components/Header"
+import Header from "./components/Header";
 import Chef from "./Pages/Chef";
 import Home from "./Pages/Home";
 import Trainer from "./Pages/Trainer";
 import Economist from "./Pages/Economist";
 import Signup from "./Pages/Signup";
 import Login from "./Pages/Login";
-import { useLogout } from "./hooks/useLogout";
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+// import { useLogout } from "./hooks/useLogout";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const App = () => {
-  const {logout} = useLogout()
-  const handleClick = () => {
-    logout()
-  };
   return (
     <div>
       <BrowserRouter>
-        <nav>
-          <h1>HomeAI</h1>
-          <Link to="/">Home</Link>
-          <Link to="/chef">Chef</Link>
-          <Link to="/trainer">Trainer</Link>
-          <Link to="/economist">Economist</Link>
-          <div>
-            <button onClick={handleClick}>Logout</button>
-          </div>
-          <div>
-            <Link to="/login">Login</Link>
-            <Link to="/signup">signup</Link>
+        <Header />
 
-          </div>
-        </nav>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,29 @@
-import icon from "../assets/icon.png";
+// import icon from "../assets/icon.png";
+import { useLogout } from "../hooks/useLogout";
+import { Link } from "react-router-dom";
 const Header = () => {
+  const {logout} = useLogout()
+  const handleClick = () => {
+    logout()
+  };
   return (
-    <header className="header">
-      <img src={icon} className="logo" />
-      <h1 className="title">Chef Claude</h1>
+    // <header className="header">
+    <header>
+        <nav>
+          <h1>HomeAI</h1>
+          <Link to="/">Home</Link>
+          <Link to="/chef">Chef</Link>
+          <Link to="/trainer">Trainer</Link>
+          <Link to="/economist">Economist</Link>
+          <div>
+            <button onClick={handleClick}>Logout</button>
+          </div>
+          <div>
+            <Link to="/login">Login</Link>
+            <Link to="/signup">signup</Link>
+
+          </div>
+        </nav>
     </header>
   );
 };
