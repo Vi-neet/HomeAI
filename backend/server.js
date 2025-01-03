8b32499fe7272bda9e29b2d7b8cd43eb6a05dda8
Vineet Bhasin <bhasinvineet49@gmail.com>
2025-01-03 19:56:02 +0530

    Minor changes

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 //     origin:`${process.env.FRONTEND_URL}`
 // }));
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin",'https://homie-black.vercel.app/');
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
