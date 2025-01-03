dfdb19a7f4b55b8bb0883d5ebd97e4e4b12e5770
Vineet Bhasin <bhasinvineet49@gmail.com>
2025-01-03 13:56:03 +0530

    Minor fixes

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,12 @@ const app = express();
 app.use(cors({
     origin:`${process.env.FRONTEND_URL}`
 }));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(express.json());
 app.use((req, res, next) => {
   console.log(req.path, req.method);
