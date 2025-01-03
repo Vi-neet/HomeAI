97a9485e42b325fa3d5a675d2b23e7aa815e5598
Vineet Bhasin <bhasinvineet49@gmail.com>
2025-01-03 13:37:32 +0530

    Setting up cors

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,12 @@ app.use(cors());
 app.use(express.json());
 app.use((req, res, next) => {
   console.log(req.path, req.method);
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 //Routes
