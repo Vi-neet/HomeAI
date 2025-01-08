a268979a8d0fd48859e120b8ba4a330bd5058a2d
Vineet Bhasin <bhasinvineet49@gmail.com>
2025-01-08 11:29:30 +0530

    Styled error message

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -70,11 +70,30 @@ const Login = () => {
           >
             Log in
           </button>
-          {error && <div className="error">{error}</div>}
+          {error && (
+            <div className="p-4 mb-4 text-sm font-medium text-red-800 bg-red-100 rounded-lg border border-red-200 dark:bg-red-900/10 dark:text-red-400 dark:border-red-800/20">
+              <div className="flex items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-5 h-5 mr-2"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+                {error}
+              </div>
+            </div>
+          )}{" "}
         </form>
         <p className="mt-10 text-center text-sm/6 text-gray-500">
           Don&#39;t have an account?{" "}
-          <Link to="/signup"
+          <Link
+            to="/signup"
             className="font-semibold text-indigo-600 hover:text-indigo-500"
           >
             Signup
--- frontend/src/Pages/Signup.jsx
+++ frontend/src/Pages/Signup.jsx
@@ -60,15 +60,32 @@ const Signup = () => {
               />
             </div>
           </div>
-          <div>
+          
             <button
               disabled={isLoading}
               className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            >
+              >
               Sign up
             </button>
-            {error && <div className="error">{error}</div>}
-          </div>
+            {error && (
+              <div className="p-4 mb-4 text-sm font-medium text-red-800 bg-red-100 rounded-lg border border-red-200 dark:bg-red-900/10 dark:text-red-400 dark:border-red-800/20">
+                <div className="flex items-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-5 h-5 mr-2"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                  {error}
+                </div>
+              </div>
+            )}{" "}
         </form>
         <p className="mt-10 text-center text-sm/6 text-gray-500">
           Already have an account?{" "}
